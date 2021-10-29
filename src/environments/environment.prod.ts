3466a872f0608a3a620bc26e8de8b3d144a54ccd
MonMonMnemonics <mikatzukiathanas@gmail.com>
2021-10-29 21:16:49 +0900

    push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   DBConn: 'https://api.mchatx.org',
   DBConn2: 'https://api.mchatx.org',
-  DBConn3: 'https://api.mchatx.org'
+  DBConn3: 'https://api.mchatx.org/ChatProxy'
 };
